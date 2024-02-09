1400da08378ea06970dada15e820b24edacda296
juan-aguilera <158538464+juan-aguilera@users.noreply.github.com>
2024-02-08 21:25:20 -0500

    search_in_array update

---
+++ app.py
@@ -0,0 +1,8 @@
+import sys
+from search_in_array import  algorithms
+from search_in_array import execution_time_algorithm
+
+#if __name__ == "__main__":
+table = execution_time_algorithm.take_execution_time(1000,10000,500,5)
+for row in table_row:
+    print(row)
